e2bd584c1f2cf9256182911b177a0a66b7d68379
huangdihd <hd20100104@163.com>
2026-05-09 13:14:57 +0800

    docs: add conceptual explanation of backpropagation

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -40,7 +40,8 @@ export default defineConfig({
           items: [
             { text: '概览', link: '/ml/supervised/' },
             { text: '基本概念', link: '/ml/supervised/what-is' },
-            { text: '梯度下降', link: '/ml/supervised/gradient-descent' }
+            { text: '梯度下降', link: '/ml/supervised/gradient-descent' },
+            { text: '反向传播', link: '/ml/supervised/backpropagation' }
           ]
         },
         {
